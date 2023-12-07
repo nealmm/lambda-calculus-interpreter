ced01da2f3ee842473f9369021156cf87e03b6dd
Matt Neal <neal.matt.m@gmail.com>
2023-12-06 21:18:48 -0500

    Setup deployment workflow

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import './style.css';
 
 import { Terminal }            from 'xterm';
+import { FitAddon }            from 'xterm-addon-fit';
 import { LineEditAddon }       from './LineEditAddon';
 import { parseExp }            from './parser';
 import { Expression, evalExp } from './evaluator';
 import { printExp }            from './printer';
 
 const terminal: Terminal = new Terminal();
+const fitAddon: FitAddon = new FitAddon();
 const editor: LineEditAddon = new LineEditAddon();
 
+terminal.loadAddon(fitAddon);
 terminal.loadAddon(editor);
 terminal.open(document.getElementById('xterm-container')!);
+fitAddon.fit();
 
 async function main() {
   while (true) {
